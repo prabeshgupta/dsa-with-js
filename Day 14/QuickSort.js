87a618aa0e24a41fdd62ccc5e3b8f1a4986bfb2e
prabeshgupta <prabeshtech232@gmail.com>
2025-05-06 07:51:57 -0700

    Quick Sort

---
+++ Day 14/QuickSort.js
@@ -0,0 +1,39 @@
+// Quick Sort - Pivot and Partition - All elements smaller than pivot goes to left and all elements bigger than pivot goes to right
+// Pivot element we assume first, mid or last
+// Time Complexity: O(n log n) on average, O(n^2) in the worst case
+
+let arr = [3, 73, 54, 23, 26, 71, 43, 6]
+quickSort(arr, 0, arr.length - 1)
+console.log(arr)
+
+function findPivot(arr, first, last) { // O(n)
+    let pivot = arr[first], i = first + 1, j = last;
+    while (i <= j) {
+        // Increment i if current element is smaller than pivot element
+        while (arr[i] <= pivot && i <= last) i++;
+        // Decrement j if current element is greater than pivot element
+        while (arr[j] > pivot && j >= first) j--;
+
+        // When i and j stop
+        if (i < j) {
+            swap(arr, i, j);
+        }
+    }
+    // The moment i is bigger than j, swap pivot with j
+    swap(arr, j, first);
+    return j;
+}
+
+function swap(arr, a, b) {
+    let temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+}
+
+function quickSort(arr, first, last) { //O(logn)
+    if (first >= last) return arr
+    let pivotIndex = findPivot(arr, first, last)
+    quickSort(arr, first, pivotIndex - 1)
+    quickSort(arr, pivotIndex + 1, last)
+    return arr
+}
